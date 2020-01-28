4507d5555fd2d1ba1a2c4435972b2fd85796389f
Jarielbrown <58567797+Jarielbrown@users.noreply.github.com>
2020-01-28 15:01:46 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        title: "How long has Tony Stark been without water and food for on the banatar with?",
+        choices: ["None, he has food and water", "4 days", "10 days", "He has water but no food"],
+        answer: "4 days"
+    },
+    {
+        title: "Why is Tony talking to his scarred Iron Man helmet?",
+        choices: ["Recording a message for Pepper Potts", "He's another Kylo Ren", "He's gone crazy", "Contacting help from Cap"],
+        answer: "Recording a message for Pepper Potts"
+    },
+    {
+        title: " What is the name of the planet that Thanos has settled on?",
+        choices: ["Titan 2", "Thanos World", "Thanos Land", "Infinity Planet"],
+        answer: "Titan 2"
+    },
+    {
+        title: "What does Thanos do in the comics after the snap?",
+        choices: ["He settled down and became a farmer", "He went out to kill all heroes", "Destroyed the universe", "Played the saxophone"],
+        answer: "He settled down and became a farmer"
+    },
+    {
+        title: "What percentage of living things did Thanos kill in the snap on earth?",
+        choices: ["50%", "1%", "75%", "90%"],
+        answer: "50%"
+    },
+
+
+];
+//Just checking my console log :)
+console.log(questions);
